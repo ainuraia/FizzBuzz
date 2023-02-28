47635110a55901d057d5320f762ca2feb19b1e99
ainuraia <116380092+ainuraia@users.noreply.github.com>
2023-02-28 15:57:48 +0100

    FizzBuzz

--- js/site.js
+++ js/site.js
@@ -1,146 +1,77 @@
-//1. Get value to test Fizz
-    //DO:
-    //Use "let" to declare a variable "fizzValue".  
-    //Assign it the value that you find in the the Html element
-    //with the id "fizzValue".  HINT: Use document.getElementById
-    
+// Get the values from the user
+function getValues(){
 
-function getValue (){
+    // Get the values from the page
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
-    let fizzValue = getElementById("fizzValue");
+    // Parse for numbers
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+    // Check that numbers are integers
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+         // We call FizzBuzz
+        let fbArray = fizzBuzz(fizzValue, buzzValue);
+         // Call displayData and write the values to the screen
+         displayData(fbArray);
+    }else{
+        alert("You must enter an integer");
+    }
+    // We call FizzBuzz
+    let fbArray = fizzBuzz(fizzValue,buzzValue);
+    // Call displayData and write the values to the screen
 }
 
-//2. get value to test Buzz
-    //DO:
-    //Use "let" to declare a variable "buzzValue".  
-    //Assign it the value that you find in the the Html element
-    //with the id "buzzValue".  HINT: Use document.getElementById
+function fizzBuzz(fizzValue, buzzValue)
+{
 
-    let buzzValue = getElementById("buzzValue");
-    
+    let returnArray = [];
 
-    //3. check for numbers
-    //DO:
-    //Convert "fizzValue" to an integer using parseInt()
+    // Loop from 1 to 100
+    for (let i = 1; i <= 100; i++) {
+       // Check to see if divisible by both 3 and 5
+       // Check to see if divisible by fizz value 3
+       // Check to see if divisible by buzz value 5
 
-    integerFizz = parseInt(fizzValue);
-
-    
-    //4. check for numbers
-    //DO:
-    //Convert "buzzValue" to an integer using parseInt()
-
-    integerBuzz = parseInt(buzzValue);
-    //5. validate that "fizzValue" and "buzzValue" are actually Numbers.
-    //DO:
-    //Use an "if-else-statement" to test the logical conjunction (AND &&) 
-    //of the Number.isInteger() of "fizzValue" and "buzzValue"
-    {
-        //6. we call generateNumbers
-        //DO:
-        //Use "let" to declare a variable "fbData".
-        //Set it equal to the result of FizzBuzz(fizzValue, buzzValue)
+        if(i % fizzValue == 0 && i % buzzValue == 0){
+            returnArray.push("FizzBuzz");
+        } else if (i % fizzValue == 0){
+            returnArray.push("Fizz");
+         }else if(i % buzzValue == 0){
+            returnArray.push("Buzz");
+        } else{
+            returnArray.push(i);
+        }
         
-        //7. we call displayNumbers
-        //DO:
-        //Call custom function "displayData()" with "fbData" as the parameter
-        //displayData(fbData);
-
-    } else {
-        //DO:
-        //Else if "fizzValue" and "buzzValue" are not Numbers
-        //send an alert to the user with the message "You must enter integers"
-
+        
     }
 
+    returnArray;
 }
 
-//Traditional Solve Fizz Buzz with a for loop
-function FizzBuzz(value1, value2) {
-    //DO:
-    //Use "let" to declare an array variable "returnArray"
-    //Set it equal to []
+// Loop over the array and create a tablerow for each item
+function displayData(fbArray){
 
-    //DO:
-    //Use "for-loop" to count form 1 to 100 using
-    //"i" as the loop control variable
-    {
-        //DO:
-        //Use an "if-else-statement" to evaluate the parameters
-        //DO:
-        //Use an "if-else-statement" to test each number against the zero modulus(%)
-        //of the parameters "value1" (fizz) and "value2" (buzz) . (ie. i % value1 == 0)
-        //First test whether the case is true for both "value1" AND (&&) "value2"
-        {
-            //DO:
-            //If the AND case is true then "push" the value 'FizzBuzz'
-            //onto the "returnArray"
-            
-        } 
-        //DO:
-        //Else test if only the zero modulus(%)
-        //of the "value1" is true
-        {
-            //DO:
-            //If true then "push" the value 'Fizz' onto the "returnArray"
-            
-        } 
-        //DO:
-        //Else test if only the zero modulus(%)
-        //of the "value2" is true
-        {
-            //DO:
-            //If true then "push" the value 'Buzz' onto the "returnArray"
-            
-        } 
-        else
-        {
-            //DO:
-            //Else just push the number "i" onto the "returnArray"
-            
-        } 
-    }
-    
-    //DO:
-    //Finally return the variable "returnArray"
-    
-}
-
-//custom display function
-function displayData(fbData) {
-
-    //get the table body element from the page
+    // Get the table body element from the page
     let tableBody = document.getElementById("results");
 
-    //get the row from the template
+    // Get the template row
     let templateRow = document.getElementById("fbTemplate");
 
-    //clear table first
+    // Clear table first
     tableBody.innerHTML = "";
 
-    for (let i = 0; i < fbData.length; i += 5) {
-        const tableRow = document.importNode(templateRow.content, true);
-        //grab only the columns in the template
-        rowCols = tableRow.querySelectorAll("td");
-
-        rowCols[0].classList.add(fbData[i]);
+    for (let index = 0; index < fbArray.length; index += 5) {
+        let tableRow = document.importNode(templateRow.content, true)
+        // Grab use the tds  put into array
+        let rowCols = tableRow.querySelectorAll("td");
         rowCols[0].textContent = fbData[i];
-       
-        rowCols[1].classList.add(fbData[i+ 1]);
-        rowCols[1].textContent = fbData[i + 1];
-       
-        rowCols[2].classList.add(fbData[i+2]);
-        rowCols[2].textContent = fbData[i + 2];
-       
-        rowCols[3].classList.add(fbData[i+3]);
-        rowCols[3].textContent = fbData[i + 3];
-       
-        rowCols[4].classList.add(fbData[i+4]);
-        rowCols[4].textContent = fbData[i + 4];
+        rowCols[1].textContent = fbData[i+1];
+        rowCols[2].textContent = fbData[i+2];
+        rowCols[3].textContent = fbData[i+3];
+        rowCols[4].textContent = fbData[i+4];
 
         tableBody.appendChild(tableRow);
     }
+    // Add all the rows to the table 
 }
-
-    
-  
